6f142b3227d3b8cd522888ff3646c653080f8c4a
Barry Deen <barrydeen@protonmail.com>
2026-09-18 11:40:18 -0400

    chore: bump version to 1.2.4 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = baseApplicationId
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.3"
+        versionCode = 7
+        versionName = "1.2.4"
         resValue("string", "app_name", "Dark Wisp")
 
         ndk {
